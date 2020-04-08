c8c83978ab49606fe15968dd9f182d8ad5ecaf9e
chenjoshua7 <chen.joshua98@gmail.com>
2020-04-07 21:07:51 -0400

    duplicate removed

--- master_data.R
+++
@@ -1,8 +0,0 @@
-#Combine all graphs and datasets
-
-infect_ratio <- c(NA, infect_ratio)
-master_data <- cbind(data, infect_ratio)
-colnames(master_data) <- c("Weeks", "Suscept", "Infect", "Inf_Ratio")
-
-library(openxlsx)
-write.csv(data,"GPS5_master_data.csv", row.names = FALSE)
